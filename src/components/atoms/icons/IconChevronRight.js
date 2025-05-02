cfc465c3ac74942c10a3ae749e295523ecbbbe10
Web Tailor <paul@web-tailor.nl>
2025-05-02 12:38:45 +0200

    header

--- src/components/atoms/icons/IconChevronRight.js
+++ src/components/atoms/icons/IconChevronRight.js
@@ -1,18 +1,13 @@
 export default function IconChevronRight() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
-            <path
-                d="M19 3.16668C10.2555 3.16668 3.16671 10.2555 3.16671 19C3.16671 27.7445 10.2555 34.8333 19 34.8333C27.7445 34.8333 34.8334 27.7445 34.8334 19C34.8334 10.2555 27.7445 3.16668 19 3.16668Z"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path d="M15.8333 25.3333L22.1666 19L15.8333 12.6667"
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path id="Vector"
+                  d="M9 18L15 12L9 6"
                   stroke="white"
                   strokeWidth="2"
                   strokeLinecap="round"
-                  strokeLinejoin="round"/>
+                  strokeLinejoin="round"
+            />
         </svg>
     );
 }
--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -1,10 +1,23 @@
 import IconLogo from "@/components/atoms/icons/IconLogo";
+import IconChevronRight from "@/components/atoms/icons/IconChevronRight";
 
 export default function Header() {
     return (
         <header id="header">
             <div className="container flex items-center justify-between">
-                <IconLogo/>
+                <div className="bg-neutral-100 w-full flex justify-between items-center px-lg py-md rounded-full border-[1px] border-solid border-neutral-200">
+                    <a href="/">
+                        <IconLogo/>
+                    </a>
+                    <nav>
+                        <ul className="flex justify-center items-center text-[16px] font-800">
+                            <li className="px-md py-sm">About us</li>
+                            <li className="px-md py-sm">Help</li>
+                            <li className="px-md py-sm">Contact</li>
+                        </ul>
+                    </nav>
+                    <a href="http://localhost" className="button bg-primary-600 text-neutral-100">Contact <IconChevronRight/></a>
+                </div>
             </div>
         </header>
     );
